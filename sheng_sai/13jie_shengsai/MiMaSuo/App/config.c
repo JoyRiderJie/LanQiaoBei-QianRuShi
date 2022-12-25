9194f5aada901c8fb2860be13f88091d8b7b8a99
zxj <205589012@qq.com>
2022-12-25 15:49:38 +0800

    13届蓝桥杯嵌入式省赛题解

--- sheng_sai/13jie_shengsai/MiMaSuo/App/config.c
+++ sheng_sai/13jie_shengsai/MiMaSuo/App/config.c
@@ -97,18 +97,9 @@ void sysWork(void)
 		passwd_wrong_count = 0;
 	}
 	
-	sprintf(temp,"c:%d f:%6d",crrl_t,frd);
-	LCD_DisplayStringLine(Line0,(uint8_t*)temp);
-	
-	
-	sprintf(temp,"passwd:%s",Rxbuff);
-	LCD_DisplayStringLine(Line6,(uint8_t*)temp);
-	
-	sprintf(temp,"flag:%d count:%d",time7_count_start_flag,time7_count);
-	LCD_DisplayStringLine(Line7,(uint8_t*)temp);
-	
-	sprintf(temp,"data:%s len:%d",passwd,strlen(passwd));
-	LCD_DisplayStringLine(Line8,(uint8_t*)temp);
+//	//显示当前密码
+//	sprintf(temp,"passwd:%s",passwd);
+//	LCD_DisplayStringLine(Line8,(uint8_t*)temp);
 }
 
 /***非阻塞模式下定时器中断回调函数***/
@@ -129,22 +120,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 	if(huart->Instance == USART1){
 		// 重新使能中断
 		HAL_UART_Receive_IT(huart,(uint8_t *)&Rxbuff,sizeof(Rxbuff)); 
-	}
-}
-
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
-{
-
-	if(htim->Instance == TIM3)
-	{
-		//读取定时器的计数值
-		crrl_t = HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1);
-		//将定时器的计数值设置成0
-		__HAL_TIM_SetCounter(htim,0);
-		//计算频率
-		frd = (80000000/80)/crrl_t;
-		//重新开启定时器
-		HAL_TIM_IC_Start_IT(htim,TIM_CHANNEL_1);
 	}
 }
 
--- sheng_sai/13jie_shengsai/MiMaSuo/Src/tim.c
+++ sheng_sai/13jie_shengsai/MiMaSuo/Src/tim.c
@@ -72,50 +72,7 @@ void MX_TIM2_Init(void)
   HAL_TIM_MspPostInit(&htim2);
 
 }
-/* TIM3 init function */
-void MX_TIM3_Init(void)
-{
 
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_IC_InitTypeDef sConfigIC = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 80-1;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_IC_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
-  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
-  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-  sConfigIC.ICFilter = 0;
-  if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-	HAL_TIM_IC_Start_IT(&htim3,TIM_CHANNEL_1);
-  /* USER CODE END TIM3_Init 2 */
-
-}
 /* TIM7 init function */
 void MX_TIM7_Init(void)
 {
