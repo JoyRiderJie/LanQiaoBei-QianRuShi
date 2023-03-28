d74ce3edf937045cba0b73483b065c691bed81f9
zxj <205589012@qq.com>
2023-03-28 22:04:40 +0800

    update(delete.bat):修改脚本文件，留下*.hex文件

--- delete.bat
+++ delete.bat
@@ -4,11 +4,18 @@ del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.o
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.axf
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.htm
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.bak
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.hex
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.lnp
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.map
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.sct
 del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.dep
+del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.lib
+del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.Src
+del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.iex
+del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.rar
+del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.zip
+
+
+
 
 
 
