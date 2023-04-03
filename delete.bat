e8e5b61089cb2e01bd3836231ed75a0f07b3f763
zxj <205589012@qq.com>
2023-04-03 10:21:15 +0800

    update(delete.bat):更新文件中删除路径，增强其兼容性

--- delete.bat
+++ delete.bat
@@ -1,18 +1,18 @@
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.crf
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.d
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.o
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.axf
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.htm
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.bak
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.lnp
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.map
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.sct
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.dep
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.lib
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.Src
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.iex
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.rar
-del /s /q D:\workPlace\keil5_text\LanQiaoBei-QianRuShi\*.zip
+del /s /q ..\*.crf
+del /s /q ..\*.d
+del /s /q ..\*.o
+del /s /q ..\*.axf
+del /s /q ..\*.htm
+del /s /q ..\*.bak
+del /s /q ..\*.lnp
+del /s /q ..\*.map
+del /s /q ..\*.sct
+del /s /q ..\*.dep
+del /s /q ..\*.lib
+del /s /q ..\*.Src
+del /s /q ..\*.iex
+del /s /q ..\*.rar
+del /s /q ..\*.zip
 
 
 
