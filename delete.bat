bc86852402d279eac4fb447447e082dfaef7951f
zxj <205589012@qq.com>
2023-04-08 19:52:52 +0800

    delete(delete.bat):删除脚本文件中删除.axf文件的语句

--- delete.bat
+++ delete.bat
@@ -1,18 +1,17 @@
-del /s /q ..\*.crf
-del /s /q ..\*.d
-del /s /q ..\*.o
-del /s /q ..\*.axf
-del /s /q ..\*.htm
-del /s /q ..\*.bak
-del /s /q ..\*.lnp
-del /s /q ..\*.map
-del /s /q ..\*.sct
-del /s /q ..\*.dep
-del /s /q ..\*.lib
-del /s /q ..\*.Src
-del /s /q ..\*.iex
-del /s /q ..\*.rar
-del /s /q ..\*.zip
+del /s /q .\*.crf
+del /s /q .\*.d
+del /s /q .\*.o
+del /s /q .\*.htm
+del /s /q .\*.bak
+del /s /q .\*.lnp
+del /s /q .\*.map
+del /s /q .\*.sct
+del /s /q .\*.dep
+del /s /q .\*.lib
+del /s /q .\*.Src
+del /s /q .\*.iex
+del /s /q .\*.rar
+del /s /q .\*.zip
 
 
 
